e620e13665f60cd077d2c27fe686145d037e8825
eSuardOdin <esuardodin@gmail.com>
2024-02-06 16:50:59 +0100

    Test menu

--- mvc-app/app/Views/utilisateurs/menu.php
+++ mvc-app/app/Views/utilisateurs/menu.php
@@ -1,8 +1,12 @@
 
 <pre>
-<?= print_r($_SESSION)?>
+<!--<?= print_r($_SESSION['logged_user'])?>-->
 </pre>
+<h2>Bienvenue <?= $_SESSION['logged_user']['user_data']->prénom_utilisateur?> <?= $_SESSION['logged_user']['user_data']->nom_utilisateur?></h2>
+<p>
+    Rôle : <?= $_SESSION['logged_user']['role']?>
+</p>
 <br/><br/>
-<form method="post" action="/logout">
+<form method="post" action="/logout">   
     <input type="submit" value="Déconnexion"/>
 </form>
