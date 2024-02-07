ea513af3bf2643af2f9a55af8e001304c8b1022e
eSuardOdin <esuardodin@gmail.com>
2024-02-07 08:40:21 +0100

    Ajout navbar

--- mvc-app/app/Views/utilisateurs/menu.php
+++ mvc-app/app/Views/utilisateurs/menu.php
@@ -1,8 +1,22 @@
 
 <pre>
-<!--<?= print_r($_SESSION['logged_user'])?>-->
+<!-- <?= print_r($_SESSION['logged_user'])?> -->
 </pre>
+<?php 
+    if($_SESSION['logged_user']['role'] === 'élève')
+    {
+        // echo APPPATH . 'Views/utilisateurs/élève/navbar.php';
+        include_once(APPPATH . 'Views/utilisateurs/élève/navbar.php');
+    } 
+
+?>
+
 <h2>Bienvenue <?= $_SESSION['logged_user']['user_data']->prénom_utilisateur?> <?= $_SESSION['logged_user']['user_data']->nom_utilisateur?></h2>
+<?php if(isset($_SESSION['logged_user']['chef'])) :?>
+    <p>
+        <u>Chef du département <?= $_SESSION['logged_user']['chef']->nom_département ?></u>
+    </p>
+<?php endif;?>
 <p>
     Rôle : <?= $_SESSION['logged_user']['role']?>
 </p>
---
+++ mvc-app/app/Views/utilisateurs/élève/navbar.php
@@ -0,0 +1,8 @@
+<nav>
+  <ul>
+    <li><a href="">Accueil</a></li>
+    <li><a href="">Produits</a></li>
+    <li><a href="">Services</a></li>
+    <li><a href="">Contact</a></li>
+  </ul>
+</nav>
