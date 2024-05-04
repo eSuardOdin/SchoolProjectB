28288c83262ad81cb096b6c18ca0f007e271618a
eSuardOdin <esuardodin@gmail.com>
2024-05-04 19:48:25 +0200

    Bug si on change l'heure après la selection de tous les champs de form créneau, mais goodenough là

--- ci_1.0/app/Views/utilisateurs/professeur/chef/add_creneau.php
+++ ci_1.0/app/Views/utilisateurs/professeur/chef/add_creneau.php
@@ -1,15 +1,45 @@
 <script>
     document.addEventListener('DOMContentLoaded', async function() {
         // ** --------- Variables du formulaire --------- **
-        const cycles = document.getElementById('cycles');
-        const matière = document.getElementById('matière');
-        const heure_créneau = document.getElementById('heure_créneau');
-        const jour_créneau = document.getElementById('jour_créneau');
+        const form =            document.getElementById('creneau_form');
+        const valid_btn =       document.getElementById('créer_créneau');
+        const cycles =          document.getElementById('cycles');
+        const matière =         document.getElementById('matière');
+        const heure_créneau =   document.getElementById('heure_créneau');
+        const jour_créneau =    document.getElementById('jour_créneau');
+        const professeur =      document.getElementById('professeur');
+        const salle =           document.getElementById('salle');
         // On lance la recherche de matières liées au cycle
         await getMatières(cycles.value);
         getHoraires(matière.value.substring(matière.value.indexOf('-')+1));
         
         // ** --------- Event listeners --------- **
+        // Savoir si le formulaire est complété
+        form.addEventListener('change', () => {
+            valid_btn.disabled = !(
+                cycles.value        != '-0' &&
+                matière.value       != '-0' &&
+                heure_créneau.value != '-0' &&
+                jour_créneau.value  != '-0' &&
+                professeur.value    != '-0' &&
+                salle.value         != '-0'
+            );
+            console.log('cycles : ' + cycles.value);
+            console.log('matière : ' + matière.value);
+            console.log('jour : ' + jour_créneau.value);
+            console.log('heure : ' + heure_créneau.value);
+            console.log('professeur : ' + professeur.value);
+            console.log('salle : ' + salle.value);
+            console.log("Valeur de vérité : " + (
+                cycles.value        != '-0' &&
+                matière.value       != '-0' &&
+                heure_créneau.value != '-0' &&
+                jour_créneau.value  != '-0' &&
+                professeur.value    != '-0' &&
+                salle.value         != '-0'
+            ));
+            console.log('______________');
+        });
         // Obtenir la liste des matières liées au cycle
         cycles.addEventListener('change', () => {
             getMatières(cycles.value);
@@ -19,12 +49,12 @@
             console.log("durée : " + matière.value.substring(matière.value.indexOf('-')+1));
             getHoraires(matière.value.substring(matière.value.indexOf('-')+1));
         });
-        // Obtenir les profs disponibles (pour l'instant, tous les profs qui enseignent la matière)
+        // Obtenir les profs disponibles
         heure_créneau.addEventListener('change', () => {
             if(matière.value != '-0' && heure_créneau.value != '-0')
             {
                 var val = matière.value;
-                getProfesseurs(
+                getProfesseursAndSalles(
                     val.substring(0, val.indexOf('-')), val.substring(val.indexOf('-')+1)
                 );
             }
@@ -33,7 +63,7 @@
             if(matière.value != '-0' && heure_créneau.value != '-0')
             {
                 var val = matière.value;
-                getProfesseurs(
+                getProfesseursAndSalles(
                     val.substring(0, val.indexOf('-')), val.substring(val.indexOf('-')+1)
                 );
             }
@@ -44,7 +74,7 @@
             if(matière.value != '-0' && heure_créneau.value != '-0')
             {
                 var val = matière.value;
-                getProfesseurs(
+                getProfesseursAndSalles(
                     val.substring(0, val.indexOf('-')), val.substring(val.indexOf('-')+1)
                 );
             }
@@ -98,6 +128,12 @@
      */
     function getHoraires(durée)
     {
+        if(matière.value == '-0')
+        {
+            heure_créneau.disabled = true;
+            return;
+        }
+        heure_créneau.disabled = false;
         const xhttp = new XMLHttpRequest();
         if(durée > 0 && durée != "----")
         {
@@ -144,7 +180,7 @@
     }
 
 
-    function getProfesseurs(id_matière, durée)
+    function getProfesseursAndSalles(id_matière, durée)
     {
         const xhttp = new XMLHttpRequest();
         xhttp.open("GET", "/show_profs?id_matière="+id_matière+"&durée="+durée+"&h_début="+heure_créneau.value+"&jour="+jour_créneau.value);
@@ -153,8 +189,42 @@
             {
                 // La requête a réussi
                 var res = xhttp.responseText.substring(0, xhttp.responseText.indexOf("<") - 1);
-                var c = JSON.parse(res);
-                console.log(c);
+                var available = JSON.parse(res);
+                // ---- Remplissage des champs du form prof -----
+                professeur.innerHTML = '<option value="-0">-----</option>';
+                // Si pas de prof dispo
+                if(available['professeurs'].length == 0)
+                {
+                    professeur.disabled = true;
+                }
+                // Profs dispos
+                else 
+                {
+                    professeur.disabled = false;
+                    available['professeurs'].forEach(prof => {
+                        var opt = document.createElement("option");
+                        opt.value = prof["id_utilisateur"];
+                        opt.text = prof["nom_utilisateur"] + " " + prof["prénom_utilisateur"];
+                        professeur.appendChild(opt);
+                    });
+                }
+                // ---- Remplissage des champs du form salle -----
+                salle.innerHTML = '<option value="-0">-----</option>';
+                if(available['salles'].length == 0)
+                {
+                    salle.disabled = true;
+                }
+                // Salles dispos
+                else 
+                {
+                    salle.disabled = false;
+                    available['salles'].forEach(s => {
+                        var opt = document.createElement("option");
+                        opt.value = s["id_salle"];
+                        opt.text = s["nom_salle"];
+                        salle.appendChild(opt);
+                    });
+                }
             }
             else
             {
@@ -171,7 +241,7 @@
 <form method="post" action="/cours">
     <input type="submit" value="Retour"/>
 </form>
-<form method="post" action="/cours/add_matière/traitement">
+<form method="post" action="/cours/add_matière/traitement" id="creneau_form">
     <select id="cycles">
     <?php
     // Session
@@ -198,7 +268,18 @@
         <option value="4">Vendredi</option>
     </select>
     <label for="heure_créneau">Horaire de début : </label>
-    <select id="heure_créneau" name="heure_créneau">
+    <select id="heure_créneau" name="heure_créneau" disabled>
+        <option value="-0">-----</option>
+    </select>
+    <br/>
+    <br/>
+    <label for="salle">Salle : </label>
+    <select id="salle" name="salle" disabled>
+        <option value="-0">-----</option>
+    </select>
+    <br/>
+    <label for="professeur">Professeur : </label>
+    <select id="professeur" name="professeur" disabled>
         <option value="-0">-----</option>
     </select>
     <br/>
